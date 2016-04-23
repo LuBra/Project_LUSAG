0ea18558e8a95111f6804fb96a15c7897087c56c
Lukas <Lukas.Braungart2@googlemail.com>
2016-04-23 16:07:31 +0200

    Initial commit
    -check_build() kommentiert (Spielfeld)
    -getBebaubar() kommentiert (World)
    
    -Alle Tiles in den Ordner Tiles verschoben und im Code angepasst.
    
    -Gras ins Inventar hinzugefügt, Kommentar wie man Items hinzu fügt
    
    -Holz ins Inventar hinzugefügt

--- src/Inventar.java
+++ src/Inventar.java
@@ -13,11 +13,18 @@ public class Inventar {
                 inventar[i][j] = new Item();
             }
         }
-        // Als erstes ID (bei blöcken die nummer, die die tiles haben, bei werkzeug die nummer die sie abbauen können sollen), dann Pfad zum Tile Bild
+        // Als erstes ID (bei blöcken die nummer, die die tiles haben, bei werkzeug die nummer die sie abbauen können sollen), dann Type(Block oder Werkzeug) dann Pfad zum Tile Bild
         inventar[0][0] = new Item(2,2,"Tiles//Item_2.png");
         inventar[1][0] = new Item(1,1,"Tiles//Item_1.png");
         inventar[2][0] = new Item(4,4,"Tiles//dirt.png");
         inventar[3][0] = new Item(1,4,"Tiles//grass.jpg");
+        inventar[4][0] = new Item(3,4,"Tiles//rinde.JPG");
+        /*
+            Spitzhacke: Type 1
+            Axt:        Type 2
+            Waffen:     Type 3
+            Blöcke:     Type 4
+        */
     }
 
     public int getItemType(int x, int y) {return inventar[y][x].getType();}
